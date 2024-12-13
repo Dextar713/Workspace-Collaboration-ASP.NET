6c7b741650a6e3ef68f417632940a0a3348d14cd
amadeus1996 <firstplayer484@protonmail.com>
2024-12-13 13:20:54 +0200

    add ChannelsController
    ChannelsController, Channel.cs and Index/New/Show views + validations

---
+++ Controllers/ChannelsController.cs
@@ -0,0 +1,58 @@
+﻿using Discord2.Data;
+using Discord2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discord2.Controllers
+{
+    public class ChannelsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ChannelsController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        // index 
+        public IActionResult Index()
+        {
+            var channels = from channel in db.Channels
+                           orderby channel.Id
+                           select channel;
+            ViewBag.Channels = channels;
+            return View();
+        }
+        // show
+        public ActionResult Show(int id)
+        {
+            Channel channel = db.Channels.Find(id);
+            ViewBag.Channel = channel;
+            return View();
+        }
+        // new
+        public IActionResult New()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult New(Channel c)
+        {
+            try
+            {
+                db.Channels.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+        // delete
+        public ActionResult Delete(int id)
+        {
+            Channel channel = db.Channels.Find(id);
+            db.Channels.Remove(channel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Models/Channel.cs
+++ Models/Channel.cs
@@ -7,7 +7,11 @@ namespace Discord2.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The name is required!")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The description is required!")]
+        public string Description { get; set; }
 
         public int? GroupId { get; set; }
 
